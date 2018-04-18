b8212116840ffb4edbf11f48ae2716620972e954
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-04-18 19:40:52 -0400

    Initial Commit

---
+++ init.zsh
@@ -0,0 +1,12 @@
+p6df::modules::sudo::version() { echo "0.0.1" }
+p6df::modules::sudo::deps()    { 
+	ModuleDeps=(
+		robbyrussell/oh-my-zsh:plugins/sudo
+	)
+}
+
+p6df::modules::sudo::external() { }
+
+p6df::modules::sudo::init() {
+}
+
