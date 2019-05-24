94ae071dd181afc93274dda6e916042326dff16e
Philip M. Gollucci <pgollucci@p6m7g8.com>
2019-05-24 17:03:48 -0400

    fixes

--- init.zsh
+++ init.zsh
@@ -1,20 +1,2 @@
 p6df::modules::sudo::version() { echo "0.0.1" }
-p6df::modules::sudo::deps()    { 
-	ModuleDeps=(
-		robbyrussell/oh-my-zsh:plugins/sudo
-	)
-}
-
-p6df::modules::sudo::external::brew() {
-
-  brew install sudo
-}
-
-p6df::modules::sudo::home::symlink() {
-
-  # XXX: none
-}
-
-p6df::modules::sudo::init() {
-}
-
+p6df::modules::sudo::deps()    { ModuleDeps=( robbyrussell/oh-my-zsh:plugins/sudo) }
